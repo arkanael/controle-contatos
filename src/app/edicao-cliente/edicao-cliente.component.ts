5ad3a779c58cfd621d0638679e6ffa893367e7d6
Luiz Guilherme Bandeira <arkanaek@gmail.com>
2022-11-01 09:07:14 -0300

    componente de edição de cliente

---
+++ src/app/edicao-cliente/edicao-cliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edicao-cliente',
+  templateUrl: './edicao-cliente.component.html',
+  styleUrls: ['./edicao-cliente.component.css']
+})
+export class EdicaoClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
