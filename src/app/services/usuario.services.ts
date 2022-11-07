bde6480ebafed46dffe2be38363605ac5eea4684
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-11-07 11:04:06 -0300

    função para executar a chamada de login de usuario

--- src/app/services/usuario.services.ts
+++ src/app/services/usuario.services.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Usuario } from "../models/usuario.model";
+import { Auth } from "../models/auth.model";
 import { environment } from "src/environments/environment";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
@@ -18,5 +19,10 @@ export class UsuarioService{
     postRegister(data:any) :Observable<Usuario>{
         return this.httpCliente.post<Usuario>(`${environment.apiContatos}/api/register`, data);
     } 
+
+    //função para executar a chamada de login de usuario na API
+    postLogin(data: any): Observable<Auth>{
+        return this.httpCliente.post<Auth>(`${environment.apiContatos}/api/login`, data);
+    }
 
 }
