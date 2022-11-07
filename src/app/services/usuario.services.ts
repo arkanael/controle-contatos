b1338b59633be42a91ae40e26b9cea659a2ba3b5
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-11-07 08:14:50 -0300

    função post para cadastrar o usuario

---
+++ src/app/services/usuario.services.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { Usuario } from "../models/usuario.model";
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UsuarioService{
+
+    //construtor com declaração das injeções de dependencia
+    constructor(private httpCliente: HttpClient){
+
+    }
+
+    //função para executar a chamada de cadastro de usuario na api
+    postRegister(data:any) :Observable<Usuario>{
+        return this.httpCliente.post<Usuario>(`${environment.apiContatos}/register`, data);
+    } 
+
+}
