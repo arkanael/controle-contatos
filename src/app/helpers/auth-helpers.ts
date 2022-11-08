438b876819457050fc24395f27ea2ae32b38e249
Luiz Guilherme Bandeira <arkanael@gmail.com>
2022-11-07 23:20:54 -0300

    criando o AuthHelper para login

---
+++ src/app/helpers/auth-helpers.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { Auth } from "../models/auth.model";
+
+@Injectable({providedIn: 'root'})
+export class AuhHelper{
+
+    //função para gravar os dados do usuário autenticado ma local storage do navegador
+    singIn(auth: Auth) : void {
+        
+        //serializar os dados do objeto para json
+        let json = JSON.stringify(auth);
+
+        //gravar na local storage
+        localStorage.setItem("auth_data", json);   
+    }
+
+    //função para ler na local storage e retornar como objeto
+    getAuthData(): Auth | null {
+
+        //ler o conteudo gravado na local storage
+        let json = localStorage.getItem("auth_data");
+
+        //verificar se o valor não é vazio
+        if(json != null){
+            //deserializar o json para objeto
+            let auth = JSON.parse(json) as Auth;
+
+            //retornando o objeto
+            return auth;
+        }else{
+            return null;
+        }
+    }
+
+    //fução para apagar o conteudo gravado do usuário na local storage 
+    signOut(): void{
+        //apagar o conteudo da local storage
+        localStorage.removeItem("auth_data");
+    }
+}
